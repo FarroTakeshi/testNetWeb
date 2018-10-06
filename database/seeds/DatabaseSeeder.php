a5dd453707e43643098eede4ebc5ae0612d3ea31
takeshifh <takeshi.farro@gmail.com>
2018-10-06 18:01:58 -0500

    seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         DB::statement('SET FOREIGN_KEY_CHECKS=0;');
 
+        $this->call(RolesTableSeeder::class);
         $this->call(UsersTableSeeder::class);
 
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,16 +14,20 @@ class UsersTableSeeder extends Seeder
         DB::table('users')->truncate();
         DB::table('users')->insert([
             [
-                'id'       => 1,
-                'username' => 'admin1',
-                'pass'     => bcrypt('secret'),
-                'role_id'  => 1,
+                'id'         => 1,
+                'username'   => 'admin1',
+                'first_name' => 'Takeshi',
+                'last_name'  => 'F',
+                'password'   => bcrypt('secret'),
+                'role_id'    => 1,
             ],
             [
-                'id'       => 2,
-                'username' => 'client1',
-                'pass'     => bcrypt('secret'),
-                'role_id'  => 2,
+                'id'         => 2,
+                'username'   => 'client1',
+                'first_name' => 'Lizbeth',
+                'last_name'  => 'M',
+                'password'   => bcrypt('secret'),
+                'role_id'    => 2,
             ]
         ]);
     }
